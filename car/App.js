0a8987728fca8a500b4532251a426a4ea5f27cba
shabana-shabu <75524151+shabana-shabu@users.noreply.github.com>
2021-10-30 22:10:59 +0530

    Add files via upload

---
+++ car/App.js
@@ -0,0 +1,15 @@
+
+import App from "./App";
+import CarDetails from "./CarDetails";
+  
+import React from 'react'
+
+const Cars= () => {
+  return (
+  
+      <div>  
+          <CarDetails component={App} />
+      </div>
+  )
+  }
+export default Cars;
